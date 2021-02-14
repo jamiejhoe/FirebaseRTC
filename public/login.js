c90cd52ae110c94f6c3c41e913339e919e4988ed
ingazhur <ingazhuravlev@gmail.com>
2021-02-13 19:09:19 -0800

    test?

--- public/login.js
+++ public/login.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
-    console.log(app);
 });
 
 function googleLogin() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
+    const provider = new firebase.auth().GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
             .then(result => {
                 const user = result.user;
                 document.write(`Hello ${user.displayName}`);
