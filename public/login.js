3a69ccb2939b2241cafe16b12d528d5b4608be38
ingazhur <ingazhuravlev@gmail.com>
2021-02-13 20:28:26 -0800

    changing onclick to @click in html

--- public/login.js
+++ public/login.js
@@ -1,10 +1,14 @@
+import * as firebase from 'firebase';
+
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
 });
 
 function googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider();
+
     firebase.auth().signInWithPopup(provider)
+
             .then(result => {
                 const user = result.user;
                 document.write(`Hello ${user.displayName}`);
